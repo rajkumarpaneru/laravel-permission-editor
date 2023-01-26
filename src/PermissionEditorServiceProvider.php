3ae650cb192b7f01f23a37518334c4c61d6059ca
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-01-26 20:59:08 +0545

    register the routes

--- src/PermissionEditorServiceProvider.php
+++ src/PermissionEditorServiceProvider.php
@@ -3,26 +3,21 @@
 namespace rajkumarpaneru\LaravelPermissionEditor;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class PermissionEditorServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Route::prefix('permission-editor')
+            ->as('permission-editor.')
+            ->group(function () {
+                $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+            });
     }
 }
